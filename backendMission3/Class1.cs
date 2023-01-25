227b20d35590a381a3ccf4ca70e2f093e398da46
ctyoung4ever <79294074+ctyoung4ever@users.noreply.github.com>
2023-01-25 15:55:57 -0700

    new stuff added: try loop for if the user inputs something not in the array added comments fixed functionality should be ready to submit.

--- backendMission3/Class1.cs
+++ backendMission3/Class1.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//namespace
 namespace backendMission3
 {
+
+    //class
     class Class1
     {
+
+        //method
         public void printBoard(char[] arr)
         {
 
+            //code for the printing of board
             Console.WriteLine("     |     |    ");
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", arr[0], arr[1], arr[2]);
             Console.WriteLine("_____|_____|_____");
@@ -22,10 +28,15 @@ namespace backendMission3
             Console.WriteLine("     |     |    ");
             Console.WriteLine();
         }
+
+        //method to decide winner
         public  int decideWinner(char[] arr)
         {
+
+            //initiate the variables
             int won = 0;
             char winningChar = 'N';
+
             //Horitzontal win conditions
             if (arr[0] == arr[1] && arr[1] == arr[2])
             {
@@ -33,17 +44,20 @@ namespace backendMission3
                 won = 1;
 
             }
+
+            //second row
             else if (arr[3] == arr[4] && arr[4] == arr[5])
             {
                 winningChar = arr[3];
                 won = 1;
             }
+
+            //third row
             else if (arr[6] == arr[7] && arr[7] == arr[8])
             {
                 winningChar = arr[6];
                 won = 1;
             }
-
 
             //Vertical Win conditions
             else if (arr[0] == arr[3] && arr[3] == arr[6])
@@ -51,42 +65,54 @@ namespace backendMission3
                 winningChar = arr[0];
                 won = 1;
             }
+
+            //second column
             else if (arr[1] == arr[4] && arr[4] == arr[7])
             {
                 winningChar = arr[1];
                 won = 1;
             }
+
+            //third column
             else if (arr[2] == arr[5] && arr[5] == arr[8])
             {
                 winningChar = arr[2];
                 won = 1;
             }
 
-            //Horizontal Win conditions
+            //diagonals
             else if (arr[0] == arr[4] && arr[4] == arr[8])
             {
                 winningChar = arr[0];
                 won = 1;
             }
+
+            //other diagonal
             else if (arr[2] == arr[4] && arr[4] == arr[6])
             {
                 winningChar = arr[2];
                 won = 1;
             }
+
             // Tie method
             else if (arr[0] != '0' && arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8')
             {
-
                 won = 2;
             }
+
+            //win returns for who won
             if (won == 1)
             {
                 Console.WriteLine("The winner is " + winningChar);
             }
+
+            //tie returns the tie
             if (won == 2)
             {
                 Console.WriteLine("Its a tie! ");
             }
+
+            //returns the won variable
             return won; 
         }
     }
--- backendMission3/Program.cs
+++ backendMission3/Program.cs
@@ -3,73 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+//namespace
 namespace backendMission3
 {
+
+    //class
     class Driver
     {
+
+        //initiate variables--array, play count for player, choice for their input, won to measure game
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8' };
         static int playCount = 1;
         static int choice;
         static int won = 0;
+        static char[] playAgain = { 'n' };
 
+        //first method
         static void Main(string[] args)
         {
+
+            //import the class from other page
             Class1 C1 = new Class1();
 
+            //welcome message
             Console.WriteLine("Welcome to Tic Tac Toe!");
 
+            //while handler to run the game until it ends
             while (true)
             {
 
-                //Console.Clear();
+                //write the instructions
                 Console.WriteLine("Player 1 is O, Player 2 is X");
                 Console.WriteLine("\n");
 
+                //print the board
                 C1.printBoard(arr);
+
+                //determine which player's turn it is
                 if (playCount % 2 == 0)
                 {
                     Console.WriteLine("Player 2 Enter your choice");
                 }
 
+                //else for player 1
                 else
                 {
                     Console.WriteLine("Player 1 Enter your choice");
-
                 }
 
-                choice = int.Parse(Console.ReadLine());
-
-                if (arr[choice] != 'X' && arr[choice] != 'O')
+                //try loop to catch errors. Originally used for when the input from the user isn't in the array of choices
+                try
                 {
-                    if (playCount % 2 == 0)
+                    //parse the user input to an int
+                    choice = int.Parse(Console.ReadLine());
+
+                    //check if the user has already chosen that slot
+                    if (arr[choice] != 'X' && arr[choice] != 'O')
                     {
-                        arr[choice] = 'X';
-                        playCount++;
+
+                        //if not already chosen, replace it with an X
+                        if (playCount % 2 == 0)
+                        {
+                            arr[choice] = 'X';
+                            playCount++;
+                        }
+
+                        //else loop for the O player
+                        else
+                        {
+                            arr[choice] = 'O';
+                            playCount++;
+                        }
+
+                        //clear console to beautify
+                        Console.Clear();
                     }
 
+                    //else handler if the space has already been chosen--returns back a message to the user
                     else
                     {
-                        arr[choice] = 'O';
-                        playCount++;
+                        Console.Clear();
+                        Console.WriteLine("Sorry, space {0} is already marked with {1}", choice, arr[choice]);
+                        Console.WriteLine("\n");
                     }
-                    Console.Clear();
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine("Sorry, space {0} is already marked with {1}", choice, arr[choice]);
-                    Console.WriteLine("\n");
-                }
-                won = C1.decideWinner(arr);
 
-                if (won != 0)
-                {
-                    break;
+                    //call the winning method which returns won as a variable
+                    won = C1.decideWinner(arr);
+
+                    //if it isn't = 0 then the game is over
+                    if (won != 0)
+                    {
+
+                        //break the loop
+                        break;
+                    }
                 }
-                
+
+                //catch handler for if the player enters something not in the array
+                //returns an error message and resets to top of the try loop
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid choice. Please enter a number between 0 and 8.");
+                    continue;
+                }
             }
+
+            //print board only used when the game is done so that you can see the end result of the board
+            C1.printBoard(arr);
             Console.ReadLine();
         }
-
     }
 }
